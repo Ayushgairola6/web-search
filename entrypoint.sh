2107c97b61cdfe251b0071f8a3f4aeb1536779c4
Ayush <Ayushgairola2002@gmail.com>
2026-05-16 19:38:50 +0530

    First commit

---
+++ entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# This script is needed to set the port to 7860 before SearXNG starts.
+# It overrides the default port (8080) to match Render's expectations.
+export SEARXNG_BIND_ADDRESS="0.0.0.0:7860"
+exec /sbin/tini -- /usr/local/searxng/dockerfiles/docker-entrypoint.sh
